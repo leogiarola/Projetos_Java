f2ce043f13e1a5a012a464d8de69f84a2a0edd37
leogiarola <leogiarola@gmail.com>
2022-06-17 22:39:54 -0300

    ClasseVenda

---
+++ Concessonaria/src/negociacao/Venda.java
@@ -0,0 +1,12 @@
+package negociacao;
+
+import java.sql.Date;
+
+import cliente.Cliente;
+import veiculo.Motocicleta;
+
+public class Venda {
+	Cliente cliente;
+	String tipoDeVeiculo;
+	Date dataVenda;
+}
